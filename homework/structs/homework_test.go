eefe8e2ff592d2bd0e897012860a8c4b54e87724
yura.parshutkin <yura.parshutkin@gmail.com>
2025-03-30 14:09:26 +0300

    add simple implementation to pack struct into 64 bytes

--- homework/structs/homework_test.go
+++ homework/structs/homework_test.go
@@ -12,79 +12,94 @@ type Option func(*GamePerson)
 
 func WithName(name string) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.mediumNumbers.Set(FirsNameByte, int(name[0]))
+		copy(person.name[:], name[1:])
 	}
 }
 
 func WithCoordinates(x, y, z int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.x = int32(x)
+		person.y = int32(y)
+		person.z = int32(z)
 	}
 }
 
 func WithGold(gold int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.gold = uint32(gold)
 	}
 }
 
 func WithMana(mana int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.mediumNumbers.Set(Mana, mana)
 	}
 }
 
 func WithHealth(health int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.mediumNumbers.Set(Health, health)
 	}
 }
 
 func WithRespect(respect int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.smallNumbers.Set(Respect, respect)
 	}
 }
 
 func WithStrength(strength int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.smallNumbers.Set(Strength, strength)
 	}
 }
 
 func WithExperience(experience int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.smallNumbers.Set(Experience, experience)
 	}
 }
 
 func WithLevel(level int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.smallNumbers.Set(Level, level)
 	}
 }
 
 func WithHouse() func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.booleans.Set(HasHouse, true)
 	}
 }
 
 func WithGun() func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.booleans.Set(HasGun, true)
 	}
 }
 
 func WithFamily() func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.booleans.Set(HasFamily, true)
 	}
 }
 
 func WithType(personType int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		// reset all prev types
+		person.booleans.Set(IsBlacksmith, false)
+		person.booleans.Set(IsWarrior, false)
+		person.booleans.Set(IsBuilder, false)
+
+		switch personType {
+		case BuilderGamePersonType:
+			person.booleans.Set(IsBuilder, true)
+		case BlacksmithGamePersonType:
+			person.booleans.Set(IsBlacksmith, true)
+		case WarriorGamePersonType:
+			person.booleans.Set(IsWarrior, true)
+		}
 	}
 }
 
@@ -95,87 +110,93 @@ const (
 )
 
 type GamePerson struct {
-	// need to implement
+	x    int32
+	y    int32
+	z    int32
+	gold uint32
+
+	mediumNumbers MediumNumbers
+	smallNumbers  SmallNumbers
+	booleans      Booleans
+	name          [41]byte
 }
 
 func NewGamePerson(options ...Option) GamePerson {
-	// need to implement
-	return GamePerson{}
+	gp := GamePerson{}
+	for i := range options {
+		options[i](&gp)
+	}
+	return gp
 }
 
 func (p *GamePerson) Name() string {
-	// need to implement
-	return ""
+	fb := byte(p.mediumNumbers.Get(FirsNameByte))
+	return string(append([]byte{fb}, p.name[:]...))
 }
 
 func (p *GamePerson) X() int {
-	// need to implement
-	return 0
+	return int(p.x)
 }
 
 func (p *GamePerson) Y() int {
-	// need to implement
-	return 0
+	return int(p.y)
 }
 
 func (p *GamePerson) Z() int {
-	// need to implement
-	return 0
+	return int(p.z)
 }
 
 func (p *GamePerson) Gold() int {
-	// need to implement
-	return 0
+	return int(p.gold)
 }
 
 func (p *GamePerson) Mana() int {
-	// need to implement
-	return 0
+	return p.mediumNumbers.Get(Mana)
 }
 
 func (p *GamePerson) Health() int {
-	// need to implement
-	return 0
+	return p.mediumNumbers.Get(Health)
 }
 
 func (p *GamePerson) Respect() int {
-	// need to implement
-	return 0
+	return p.smallNumbers.Get(Respect)
 }
 
 func (p *GamePerson) Strength() int {
-	// need to implement
-	return 0
+	return p.smallNumbers.Get(Strength)
 }
 
 func (p *GamePerson) Experience() int {
-	// need to implement
-	return 0
+	return p.smallNumbers.Get(Experience)
 }
 
 func (p *GamePerson) Level() int {
-	// need to implement
-	return 0
+	return p.smallNumbers.Get(Level)
 }
 
 func (p *GamePerson) HasHouse() bool {
-	// need to implement
-	return false
+	return p.booleans.Get(HasHouse)
 }
 
 func (p *GamePerson) HasGun() bool {
-	// need to implement
-	return false
+	return p.booleans.Get(HasGun)
 }
 
-func (p *GamePerson) HasFamilty() bool {
-	// need to implement
-	return false
+func (p *GamePerson) HasFamily() bool {
+	return p.booleans.Get(HasFamily)
 }
 
 func (p *GamePerson) Type() int {
-	// need to implement
-	return 0
+	switch {
+	case p.booleans.Get(IsBuilder):
+		return BuilderGamePersonType
+	case p.booleans.Get(IsBlacksmith):
+		return BlacksmithGamePersonType
+	case p.booleans.Get(IsWarrior):
+		return WarriorGamePersonType
+	default:
+		return BuilderGamePersonType
+	}
 }
 
 func TestGamePerson(t *testing.T) {
@@ -220,7 +241,7 @@ func TestGamePerson(t *testing.T) {
 	assert.Equal(t, experience, person.Experience())
 	assert.Equal(t, level, person.Level())
 	assert.True(t, person.HasHouse())
-	assert.True(t, person.HasFamilty())
+	assert.True(t, person.HasFamily())
 	assert.False(t, person.HasGun())
 	assert.Equal(t, personType, person.Type())
 }
