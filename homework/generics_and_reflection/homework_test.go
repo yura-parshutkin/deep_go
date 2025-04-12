b9810ccfffa404a732d94c497171caf7202d532e
yura.parshutkin <yura.parshutkin@gmail.com>
2025-04-12 21:23:55 +0300

    implementation Serializer for Properties

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +18,51 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+const delimiter = '='
+
+func Serialize(input any) string {
+	rt := reflect.TypeOf(input)
+	if rt.Kind() != reflect.Struct {
+		panic("expected struct type, actual: " + rt.Kind().String())
+	}
+	rv := reflect.ValueOf(input)
+	bf := strings.Builder{}
+	fieldsCount := rt.NumField()
+
+	for i := 0; i < fieldsCount; i++ {
+		prop, ok := rt.Field(i).Tag.Lookup("properties")
+		if !ok {
+			continue
+		}
+		key, omitempty := parseProp(prop)
+		val := fmt.Sprintf("%v", rv.Field(i))
+		// skip empty values if omitempty is set
+		if omitempty && val == "" {
+			continue
+		}
+		bf.WriteString(key)
+		bf.WriteByte(delimiter)
+		bf.WriteString(val)
+		// do not use \n for last line
+		if i != fieldsCount-1 {
+			bf.WriteByte('\n')
+		}
+	}
+	return bf.String()
+}
+
+func parseProp(prop string) (val string, omitempty bool) {
+	parts := strings.Split(prop, ",")
+	if len(parts) < 2 {
+		return prop, false
+	}
+	if parts[0] == "omitempty" {
+		return parts[1], true
+	}
+	if parts[1] == "omitempty" {
+		return parts[0], true
+	}
+	panic(fmt.Sprintf("unknown property %q", prop))
 }
 
 func TestSerialization(t *testing.T) {
